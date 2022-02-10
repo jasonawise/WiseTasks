d6a43a79977ffa850d9343f216b12c4d14d5ed30
Jason A. Wise <jasonandrewwise@gmail.com>
2022-02-09 21:37:24 -0500

    Initial Commit

---
+++ Shared/WiseTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseTasksApp.swift
+//  Shared
+//
+//  Created by Jason Wise on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WiseTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
